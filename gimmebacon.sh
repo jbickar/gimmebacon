96ae5bcb7dedea01b145e43f94412c2ec02a7f00
John Bickar <jbickar@stanford.edu>
2013-09-24 15:29:11 -0700

    Math is hard.

--- gimmebacon.sh
+++ gimmebacon.sh
@@ -3,6 +3,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if test "$1" == 1; then
   paragraphs=$1
 else
-  paragraphs=`expr $1 + 2`
+  paragraphs=`expr $1 \* 2 - 1`
 fi
 cat $dir/baconipsum.txt | head -$paragraphs | cut -d " " -f 1- | pbcopy
