ad1a574c8d6d68e9e8fdc24f045909158f17104a
John Bickar <jbickar@stanford.edu>
2013-09-24 13:52:55 -0700

    Location-agnostic. More better for aliasing

--- gimmebacon.sh
+++ gimmebacon.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if test "$1" == 1; then
   paragraphs=$1
 else
   paragraphs=`expr $1 + 2`
 fi
-cat baconipsum.txt | head -$paragraphs | cut -d " " -f 1- | pbcopy
+cat $dir/baconipsum.txt | head -$paragraphs | cut -d " " -f 1- | pbcopy
