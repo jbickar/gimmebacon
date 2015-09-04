a97d9253e28928e7fc224393fb9a1f85284182c7
Scotty Logan <swl@stanford.edu>
2015-09-04 14:22:17 -0700

    cleaned up gimmebacon.sh; added symlink detection

--- gimmebacon.sh
+++ gimmebacon.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if test "$1" == 1; then
-  paragraphs=$1
-else
-  paragraphs=`expr $1 \* 2 - 1`
-fi
-cat $dir/baconipsum.txt | head -$paragraphs | cut -d " " -f 1- | pbcopy
+target=$(readlink $0)
+target=${target:-$(dirname $0)}
+dir=$(dirname ${target})
+slices=$((${1:-5} * 2 - 1))
+cat ${dir}/baconipsum.txt | head -${slices} | pbcopy
